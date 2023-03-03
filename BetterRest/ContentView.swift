024e4ba22dd04986529afb2de50fa770ab6f3c29
SeikaHirori <SeikaHirori@outlook.com>
2023-03-03 12:22:20 -0800

    Finished section 1.4

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -12,16 +12,18 @@ struct ContentView: View {
     @State private var sleepAmount: Double = 8.0
     @State private var wakeUp:Date = Date.now
 
-    var components = DateComponents()
-    components.hour = 8 ?? 0
-    components.minute = 0 ?? 0
-    let date = Calendar.current.date(from: components) ?? Date.now
 
     var body: some View {
         VStack {
+            Text(Date.now, format: .dateTime.hour().minute())
+            Text(Date.now, format: .dateTime.day().month().year())
+            
+            Text(Date.now.formatted(date: .long, time: .shortened))
+            
             Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
             DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
                 .labelsHidden()
+            
         }
         .padding()
     }
@@ -30,6 +32,13 @@ struct ContentView: View {
         let tomorrow = Date.now.addingTimeInterval(86400)
         
         let range = Date.now...tomorrow
+    }
+    
+    func exampleComponents() {
+        var components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+        
     }
     
 }
