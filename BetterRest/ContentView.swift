669ae8383c8971c92a30e7a7acd4908348452f7b
SeikaHirori <SeikaHirori@outlook.com>
2023-03-03 13:10:39 -0800

    - moved examples of text Date into it's own struct - added state var coffeeAmount

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,16 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var sleepAmount: Double = 8.0
     @State private var wakeUp:Date = Date.now
+    @State private var sleepAmount: Double = 8.0
+    @State private var coffeeAmount: Int = 1
 
 
     var body: some View {
         VStack {
-            Text(Date.now, format: .dateTime.hour().minute())
-            Text(Date.now, format: .dateTime.day().month().year())
-            
-            Text(Date.now.formatted(date: .long, time: .shortened))
+                        
+            exampleTextDates()
             
             Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
             DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
@@ -41,6 +40,18 @@ struct ContentView: View {
         
     }
     
+}
+
+
+struct exampleTextDates: View {
+    var body: some View {
+        VStack {
+            Text(Date.now, format: .dateTime.hour().minute())
+            Text(Date.now, format: .dateTime.day().month().year())
+            
+            Text(Date.now.formatted(date: .long, time: .shortened))
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
