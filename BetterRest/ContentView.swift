6674ebae00c49075ec958c1ed2f1bf134a07a740
SeikaHirori <SeikaHirori@outlook.com>
2023-03-03 11:52:02 -0800

    added function exampleDates()

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -15,11 +15,22 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            DatePicker("Please enter a date", selection: $wakeUp)
+            DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
                 .labelsHidden()
         }
         .padding()
     }
+    
+    func exampleDates() {
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        
+        let range = Date.now...tomorrow
+    }
+    
+    
+    
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
