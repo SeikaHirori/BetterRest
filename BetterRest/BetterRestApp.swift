33df2af698d6b728ab3be04528b4eeaf4b2c9d07
SeikaHirori <SeikaHirori@outlook.com>
2023-03-02 22:26:59 -0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Seika Hirori on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
