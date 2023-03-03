b3c10f8b831520fdb15b866c77cf8fa8d2e12fd6
SeikaHirori <SeikaHirori@outlook.com>
2023-03-03 11:47:19 -0800

    Added DatePicker that's hidden

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var sleepAmount = 8.0
+    @State private var sleepAmount: Double = 8.0
+    @State private var wakeUp:Date = Date.now
     
     var body: some View {
         VStack {
             Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            DatePicker("Please enter a date", selection: $wakeUp)
+                .labelsHidden()
         }
         .padding()
     }
